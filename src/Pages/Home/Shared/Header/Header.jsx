ec7304b7d010a8c82dee644978203e1481349d18
nahidmursaline <nahid15-12408@diu.edu.bd>
2024-03-02 20:23:21 +0600

    navigation added

--- src/Pages/Home/Shared/Header/Header.jsx
+++ src/Pages/Home/Shared/Header/Header.jsx
@@ -1,9 +1,77 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+// import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../../../Assets/Logo/userManagementLogo-1.webp';
+// import { AuthContext } from '../../../Provider/AuthProvider';
 
 const Header = () => {
+
+ 
+// const {user,logOut} = useContext(AuthContext);
+
+// const handleLogOut = ()=> {
+//   logOut()
+//   .then(()=> {})
+//   .catch(error => console.log(error))
+// }
+
+
+
+
+
+
+    const navItems = 
+   
+    <>
+      <li><Link to='/'>Home</Link></li>
+     <li><Link to='/allUser'>All User</Link></li>
+     <li><Link  to='/addUser'>Add User</Link></li>
+    
+
+     
+     {/* {user?.email? <>
+      
+     
+      <li><button onClick={handleLogOut} >Log Out</button></li>
+      <Link to='/profile'><span className='pt-8 pt-[40px] text-red-500'>{user?.displayName}</span></Link>
+     
+     </>:
+    
+    <li><Link to='/login'>Login</Link></li>}
+     {
+              user && <img className='ms-4 rounded-full' title= {user.displayName}style={{"height" : "38px", "width" : "38px"}} src={user.photoURL} alt="" />
+            } */}
+    
+    </>
+    
+
+
     return (
         <div>
-            
+              <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              
+             {navItems}
+             
+            </ul>
+          </div>
+          <Link to='/' className="w-[53px] h-[53px] mr-2 rounded-full "><img style={{ width:70}} src={logo} alt="" /></Link>
+          <h5 className='text-3xl font-bold '>College Hunts</h5>
+        </div>
+          
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+           {navItems}
+          </ul>
+        </div>
+       
+      </div>
         </div>
     );
 };
---
+++ src/Pages/Home/Slider/Slider.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import slider1 from '../../../../Assets/Slider/Slider-1.jpg'
+import slider2 from '../../../../Assets/Slider/Slider-2.jpg'
+import slider3 from '../../../../Assets/Slider/Slider-3.jpg'
+
+const Slider = () => {
+    return (
+        <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={slider1}className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide4" className="btn btn-circle">❮</a> 
+            <a href="#slide2" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide2" className="carousel-item relative w-full">
+          <img src={slider2}className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide1" className="btn btn-circle">❮</a> 
+            <a href="#slide3" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+        <div id="slide3" className="carousel-item relative w-full">
+          <img src={slider3}className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a href="#slide2" className="btn btn-circle">❮</a> 
+            <a href="#slide4" className="btn btn-circle">❯</a>
+          </div>
+        </div> 
+       
+      </div>
+    );
+};
+
+export default Slider;
